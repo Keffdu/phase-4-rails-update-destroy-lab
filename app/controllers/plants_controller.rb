1c1895687e617212c4296a2c505fe60b1b4e9ba2
Kevin Dutzy <kddutz15@gmail.com>
2023-01-10 12:12:37 -0500

    commit tree f5776f6e5135ae95250bf2bdd99b1de323b8b254

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,7 +18,31 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    if plant
+      plant.update(plant_params)
+      render json: plant, status: :accepted
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
+  def destroy
+    plant =find_plant
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
   private
+
+  def find_plant
+    Plant.find_by(id: params[:id])
+end
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
